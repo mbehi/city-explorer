3f2f89fd4418de7b9ea805596220ef36509777ba
mbehi <mohsin.behi@gmail.com>
2021-04-14 22:20:59 -0500

    added event handle for the form submit, confirmed locationIQ data working when user request data on city

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import './App.css';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: ''
+    };
+  }
+  handleFormSubmit = async(event) => {
+    event.preventDefault();
+    console.log(this.state.city);
+    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`);
+    console.log(cityData);
+  }
   render() {
     return (
       <>
         <h1>City Explorer</h1>
-        <Form>
+        <Form onSubmit={this.handleFormSubmit}>
           <Form.Group controlId="city">
             <Form.Label>City Name</Form.Label>
-            <Form.Control></Form.Control>
+            <Form.Control value={this.state.city} onInput={e => this.setState({city: e.target.value})}></Form.Control>
           </Form.Group>
-          <Button variant="primary" type="submit">Explore!</Button>
+          <Button variant="primary" type="submit">
+            Explore!
+            </Button>
         </Form>
         </>
     )

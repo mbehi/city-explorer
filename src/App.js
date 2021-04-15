9073aa5e5049d84609c8e2c4b94d05133a8c5497
mbehi <mohsin.behi@gmail.com>
2021-04-14 22:02:10 -0500

    added form to collect city name from the user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 class App extends React.Component {
   render() {
     return (
       <>
         <h1>City Explorer</h1>
+        <Form>
+          <Form.Group controlId="city">
+            <Form.Label>City Name</Form.Label>
+            <Form.Control></Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">Explore!</Button>
+        </Form>
         </>
     )
   }

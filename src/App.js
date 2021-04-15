aa60f1bbeee217ff93df9cd13d6512d13b81689c
mbehi <mohsin.behi@gmail.com>
2021-04-14 21:51:45 -0500

    added class app and fragment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        </>
+    )
+  }
 }
 
 export default App;

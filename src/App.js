ddfd7da4ea2fa64224286f4687dacfed28897b65
mbehi <mohsin.behi@gmail.com>
2021-05-07 23:54:24 -0500

    add env

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends React.Component {
       });
       this.getWeatherData();
   }
-
   getWeatherData = async() => {
     try { 
       const weatherData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather`)

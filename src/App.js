14fb872e2d845eb03a971c8b313ae37a9e6b6800
mbehi <mohsin.behi@gmail.com>
2021-05-08 09:40:37 -0500

    update

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ class App extends React.Component {
         cityData: cityICareAboutData
       });
       this.getWeatherData();
-  }
-  
+  }  
   getWeatherData = async() => {
     try { 
       const weatherData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather`)

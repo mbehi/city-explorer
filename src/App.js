09a83dbc09049613d133fe61c9765c06ce9c689e
mbehi <mohsin.behi@gmail.com>
2021-05-07 23:43:05 -0500

    added env variable

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
 
   getWeatherData = async() => {
     try { 
-      const weatherData = await axios.get('http://localhost:3002/weather')
+      const weatherData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather`)
       this.setState({
         weatherData: weatherData.data
       })
